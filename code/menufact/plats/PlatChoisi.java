a0aed71c43a78ca17e7a409c854ed3ac20ab95cd
MathiasGagnon <Mathias.Gagnon@USherbrooke.ca>
2021-05-08 11:02:36 -0400

    push i gess

--- code/menufact/plats/PlatChoisi.java
+++ code/menufact/plats/PlatChoisi.java
@@ -5,10 +5,12 @@ import menufact.plats.PlatAuMenu;
 public class PlatChoisi {
     private PlatAuMenu plat;
     private int quantite;
+    private PlatEtat etat;
 
     public PlatChoisi(PlatAuMenu plat, int quantite) {
         this.plat = plat;
         this.quantite = quantite;
+        this.etat = PlatEtat.COMMANDE;
     }
 
     @Override
@@ -16,6 +18,7 @@ public class PlatChoisi {
         return "menufact.plats.PlatChoisi{" +
                 "quantite=" + quantite +
                 ", plat=" + plat +
+                ", etat =" + etat +
                 '}';
     }
 
@@ -25,6 +28,10 @@ public class PlatChoisi {
 
     public void setQuantite(int quantite) {
         this.quantite = quantite;
+    }
+
+    public void setEtat(PlatEtat etat){
+        this.etat = etat;
     }
 
     public PlatAuMenu getPlat() {
---
+++ code/menufact/plats/PlatEtat.java
@@ -0,0 +1,5 @@
+package menufact.plats;
+
+public enum PlatEtat {
+    COMMANDE, PREPARATION, TERMINE, SERVI, IMPOSSIBLE
+}
