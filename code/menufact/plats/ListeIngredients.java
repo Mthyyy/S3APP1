c63441c3295b9e6776c1a4c421afeea08f2301de
Mathieu <mat_lefebvre12@hotmail.com>
2021-05-10 13:35:09 -0400

    Update ListeIngredients.java

--- code/menufact/plats/ListeIngredients.java
+++ code/menufact/plats/ListeIngredients.java
@@ -5,21 +5,38 @@ import ingredients.exceptions.IngredientException;
 
 import java.util.ArrayList;
 
+/**
+ * liste des ingredient d'un platMenu
+ */
 public class ListeIngredients {
 
     private ArrayList<Ingredient> lesIngedients;
-    FactoryIngredient factory;
+    private FactoryIngredient factory;
 
+    /**
+     * constructeur
+     */
     public ListeIngredients()
     {
          lesIngedients = new ArrayList<Ingredient>();
          factory = new ConcreteFactoryIngredient();
     }
+
+    /**
+     * Ajoute un ingredient dans la liste
+     * @param ing ingredient
+     * @param quantite la quantite
+     * @throws IngredientException ingredient introuvable
+     */
     public void ajouterIngredient(Ingredient ing , int quantite) throws IngredientException {
 
         lesIngedients.add(factory.creerIngredient(ing.getNom(),ing.getDescription(),quantite,ing.getEtat(),ing.getType()));
     }
 
+    /**
+     * Retourne la liste des ingredients
+     * @return
+     */
     public ArrayList<Ingredient> getListIngredients()
     {
         return lesIngedients;
